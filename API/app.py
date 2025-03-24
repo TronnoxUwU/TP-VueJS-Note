455e090afec08a935242c06b27ff10534e5c6c9f
TronnoxUwU <baptisterichard98@free.fr>
2025-03-24 14:51:13 +0100

    ça marche

--- API/app.py
+++ API/app.py
@@ -4,5 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 cors = CORS(app, resources={f"/todo/api/v1.0/*": {"origins": "*"}})
 app.config["SECRET_KEY"] = "8669704c-504a-4707-aab3-fd8e561ff1bc"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../todo/db.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../API/db.sqlite"
 db = SQLAlchemy(app)
