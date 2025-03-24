bc3056156111a89e5a48aa047888c6330602dbc0
Emir-de-la-moquette <shanky.yt@gmail.com>
2025-03-24 15:33:39 +0100

    conect rest (todos)

--- TPtodo/src/App.vue
+++ TPtodo/src/App.vue
@@ -1,6 +1,8 @@
 <script>
 import TodoItem from './components/TodoItem.vue';
 
+let URL_REST = 'http://localhost:5000/todos';
+
 let data = {
     todoID: 2,
     todos: [
@@ -8,8 +10,7 @@ let data = {
         { id: 2, text: 'Apprendre REST', checked: false }
     ],
     title: 'Mes tâches',
-    newItem: '',
-    URL_REST: 'localhost:5000'
+    newItem: ''
 };
 
 export default {
@@ -22,7 +23,9 @@ export default {
             if (text) {
                 fetch(URL_REST, {
                     method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
+                    headers: { 'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin':'*'
+                     },
                     body: JSON.stringify({ text: text, checked: false })
                 })
                 .then(response => response.json())
@@ -53,7 +56,11 @@ export default {
             });
         },
         fetchTodos: function () {
-            fetch(URL_REST)
+            fetch(URL_REST,
+            {method: 'GET',
+            headers: { 'Content-Type': 'application/json',
+                      'Access-Control-Allow-Origin':'*'
+                      }})
             .then(response => response.json())
             .then(data => {
                 this.todos = data;
@@ -68,17 +75,17 @@ export default {
 </script>
 
 <template>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <div class="container">
-        <h2>{{ title }}</h2>
-        <ol>
-            <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @remove="removeItem" @edit="editItem"></TodoItem>
-        </ol>
-        <div class="input-group">
-            <input v-model="newItem" @keyup.enter="addItem" placeholder="Ajouter une tâche à la liste" type="text" class="form-control">
-            <span class="input-group-btn">
-                <button @click="addItem" class="btn btn-default" type="button">Ajouter</button>
-            </span>
-        </div>
-    </div>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+  <div class="container">
+      <h2>{{ title }}</h2>
+      <ol>
+          <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @remove="removeItem" @edit="editItem"></TodoItem>
+      </ol>
+      <div class="input-group">
+          <input v-model="newItem" @keyup.enter="addItem" placeholder="Ajouter une tâche à la liste" type="text" class="form-control">
+          <span class="input-group-btn">
+              <button @click="addItem" class="btn btn-default" type="button">Ajouter</button>
+          </span>
+      </div>
+  </div>
 </template>
--- TPtodo/src/components/TodoItem.vue
+++ TPtodo/src/components/TodoItem.vue
@@ -6,9 +6,12 @@ export default {
     methods: {
         suppr: function() {
             this.$emit('remove', { id: this.todo.id });
+        },
+        save: function() {
+            this.$emit('edit', { id: this.todo.id, text: this.todo.text });
         }
     },
-    emits: ['remove']
+    emits: ['remove', 'edit']
 }
 </script>
 
@@ -16,9 +19,11 @@ export default {
     <li :class="{ 'alert alert-success': todo.checked }">
         <div class="checkbox">
             <label>
-                <input type="checkbox" v-model="todo.checked"> {{ todo.text }}
+                <input type="checkbox" v-model="todo.checked">
+                <input type="text" v-model="todo.text">
             </label>
         </div>
-        <input type="button" class="btn btn-danger" value="Supprimer" @click="suppr">
+        <button class="btn btn-primary" @click="save">Enregistrer</button>
+        <button class="btn btn-danger" @click="suppr">Supprimer</button>
     </li>
 </template>
