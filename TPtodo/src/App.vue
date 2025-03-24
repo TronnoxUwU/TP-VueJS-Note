8ff659e41f8a75951c37f2958ee722e56715309a
Emir-de-la-moquette <shanky.yt@gmail.com>
2025-03-24 15:02:14 +0100

    pleins d'ajouts, debut connect rest sur vue

--- TPtodo/src/App.vue
+++ TPtodo/src/App.vue
@@ -1,30 +1,84 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import TodoItem from './components/TodoItem.vue';
+
+let data = {
+    todoID: 2,
+    todos: [
+        { id: 1, text: 'Faire les courses', checked: true },
+        { id: 2, text: 'Apprendre REST', checked: false }
+    ],
+    title: 'Mes tâches',
+    newItem: '',
+    URL_REST: 'localhost:5000'
+};
+
+export default {
+    data() {
+        return data;
+    },
+    methods: {
+      addItem: function () {
+            let text = this.newItem.trim();
+            if (text) {
+                fetch(URL_REST, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ text: text, checked: false })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    this.todos.push(data);
+                    this.newItem = '';
+                });
+            }
+        },
+        removeItem: function (event) {
+            fetch(`${URL_REST}/${event.id}`, { method: 'DELETE' })
+            .then(() => {
+                this.todos = this.todos.filter(todo => todo.id !== event.id);
+            });
+        },
+        editItem: function (event) {
+            fetch(`${URL_REST}/${event.id}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ text: event.text })
+            })
+            .then(response => response.json())
+            .then(data => {
+                let todo = this.todos.find(todo => todo.id === event.id);
+                if (todo) {
+                    todo.text = data.text;
+                }
+            });
+        },
+        fetchTodos: function () {
+            fetch(URL_REST)
+            .then(response => response.json())
+            .then(data => {
+                this.todos = data;
+            });
+        }
+    },
+    mounted() {
+        this.fetchTodos();
+    },
+    components: { TodoItem }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <div class="container">
+        <h2>{{ title }}</h2>
+        <ol>
+            <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" @remove="removeItem" @edit="editItem"></TodoItem>
+        </ol>
+        <div class="input-group">
+            <input v-model="newItem" @keyup.enter="addItem" placeholder="Ajouter une tâche à la liste" type="text" class="form-control">
+            <span class="input-group-btn">
+                <button @click="addItem" class="btn btn-default" type="button">Ajouter</button>
+            </span>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ TPtodo/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    props: {
+        todo: Object
+    },
+    methods: {
+        suppr: function() {
+            this.$emit('remove', { id: this.todo.id });
+        }
+    },
+    emits: ['remove']
+}
+</script>
+
+<template>
+    <li :class="{ 'alert alert-success': todo.checked }">
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" v-model="todo.checked"> {{ todo.text }}
+            </label>
+        </div>
+        <input type="button" class="btn btn-danger" value="Supprimer" @click="suppr">
+    </li>
+</template>
